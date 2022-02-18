da658eb2ba4bf6a70df6c0e611e5c8a9ed9a3706
Luis Martins <lcmartins2@stefanini.com>
2022-02-18 16:48:47 -0300

    get implemented and functioning

--- Controllers/EmployeeController.js
+++ Controllers/EmployeeController.js
@@ -4,6 +4,19 @@ const { Router } = require('express');
 const EmployeeController = Router();
 
 
+EmployeeController.get('', async (req, res) => {
+  
+  try {
+    res.status(200).json(
+      await EmployeeService.index()
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: 'EmployeeService.index() is not working.'})
+  }
+})
+
+
 EmployeeController.post('', (req, res) => {
   
   const { name, position } = req.body;
@@ -13,8 +26,12 @@ EmployeeController.post('', (req, res) => {
     res.status(201).json();
   } catch (error) {
     console.log(error);
-    res.status(500).json({error: ''})
+    res.status(500).json({error: 'EmployeeService.store is not working.'})
   }
 })
+
+
+
+
 
 module.exports = EmployeeController;
--- Services/EmployeeService.js
+++ Services/EmployeeService.js
@@ -1,10 +1,14 @@
 const EmployeeRepository = require('../Repositories/EmployeeRepository');
 
+const index = async () => {
+  return await EmployeeRepository.findAll();
+}
+
 const store = ({ name, position }) => {
   
   EmployeeRepository.insert({ name, position });
 }
 
 module.exports = {
-  store
+  index, store 
 };
